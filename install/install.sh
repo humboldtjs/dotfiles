1687cc8b54af5d05fca3723210c30806f6ca072c
Daniël Haveman <daniel@iMac-Work.local>
2013-03-29 17:53:45 +0100

    Now checks whether Xcode command line tools are installed Link the nanorc file to the user dir Make sure we use our custom actions.rb to do brew cask linkapps Remove updaters and other unwanted apps Added OSX defaults (defaults write)

--- install/install.sh
+++ install/install.sh
@@ -20,6 +20,15 @@ function brewtap {
 	fi	
 }
 
+echo "Checking pre-requisites"
+echo "-----------------------"
+if [ "`make 2>&1 >/dev/null | grep 'not found'`" == "" ]; then
+	# Xcode command line tools are note installed
+	echo "Please install the Xcode Commandline Tools first"
+	# quit here?
+fi
+echo ""
+
 echo "Installing ZSH"
 echo "--------------"
 # Check if default shell is zsh
@@ -86,6 +95,8 @@ else
 	git clone https://github.com/humboldtjs/dotfiles.git
 fi
 ln -s ~/dotfiles/casks /usr/local/Library/Taps/my-casks > null &2> null
+ln -s ~/dotfiles/settings/.nanorc ~/.nanorc > null &2> null
+
 echo ""
 
 echo "Installing desktop software"
@@ -108,7 +119,25 @@ caskinstall textmate2
 caskinstall transmission
 caskinstall vlc
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-brew cask linkapps > null &2> null
+if ! [ -f /usr/local/Library/Taps/phinze-cask/lib/cask/actions.rb.orig ]; then
+	cp /usr/local/Library/Taps/phinze-cask/lib/cask/actions.rb /usr/local/Library/Taps/phinze-cask/lib/cask/actions.rb.orig
+fi
+cp ~/dotfiles/install/actions.rb /usr/local/Library/Taps/phinze-cask/lib/cask/actions.rb
+cp ~/dotfiles/install/actions.rb /usr/local/Library/LinkedKegs/brew-cask/rubylib/cask/actions.rb
+brew cask linkapps > null 2> null
+rm -Rf /Applications/app_mode_loader.app
+rm -Rf /Applications/crash_report_sender.app
+rm -Rf /Applications/crashreporter.app
+rm -Rf /Applications/Delete\ VLC\ Preferences.app
+rm -Rf /Applications/finish_installation.app
+rm -Rf /Applications/FluidApp.app
+rm -Rf /Applications/Google\ Chrome\ Helper.app
+rm -Rf /Applications/Google\ Chrome\ Helper\ EH.app
+rm -Rf /Applications/Google\ Chrome\ Helper\ NP.app
+rm -Rf /Applications/plugin-container.app
+rm -Rf /Applications/Prototype\ Droplet.app
+rm -Rf /Applications/Relauncher.app
+rm -Rf /Applications/updater.app
 echo ""
 
 echo "Installing MAMP"
@@ -130,6 +159,48 @@ else
 
 	mkdir -p ~/Hosts
 fi
+echo ""
+
+echo "Setting OSX defaults"
+echo "--------------------"
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# Enable subpixel font rendering on non-Apple LCDs
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# Enable snap-to-grid for desktop icons
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# # Make Dock icons of hidden applications translucent
+# defaults write com.apple.dock showhidden -bool true
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+# Disable the Ping sidebar in iTunes
+defaults write com.apple.iTunes disablePingSidebar -bool true
+# Disable all the other Ping stuff in iTunes
+defaults write com.apple.iTunes disablePing -bool true
+# Reset Launchpad
+[ -e ~/Library/Application\ Support/Dock/*.db ] && rm ~/Library/Application\ Support/Dock/*.db
+# Kill affected applications
+for app in Finder Dock Mail Safari iTunes iCal Address\ Book SystemUIServer; do killall "$app" > /dev/null 2>&1; done
 echo ""
 
 echo "Opening background apps"
