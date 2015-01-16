acd9c020bfe8972b135e8b0ca2dfceecd98b3a6c
Daniël Haveman <daniel@utalk.nl>
2015-01-16 17:05:19 +0100

    Also setup cider

--- install.sh
+++ install.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 if ! [ -f "/usr/local/bin/brew" ]; then
-	echo "-------------------"
-	echo "Installing homebrew"
-	echo "-------------------"
+	echo "Installing homebrew..."
 	
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if ! [ -f "/usr/local/bin/cider" ]; then
+	echo "Installing cider..."
+	
+	pip install -U cider
+fi
+
+if ! [ -f "/usr/local/bin/git" ]; then
+	echo "Installing git..."
+	
+	brew install git
+fi
+
+if ! [ -f "~/dotfiles" ]; then
+	cd ~
+	git clone https://github.com/humboldtjs/dotfiles.git
+fi
+
+if ! [ -f "~/.cider" ]; then
+	ln -s "~/dotfiles/.cider" "~/.cider"
 fi
