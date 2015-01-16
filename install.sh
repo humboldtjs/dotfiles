2b7f7a49bddd85dfdb50e5ba7b95c0efc27419ba
Daniël Haveman <daniel@utalk.nl>
2015-01-16 17:30:14 +0100

    Brew doctor

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if ! [ -f "/usr/local/bin/brew" ]; then
 	echo "Installing homebrew..."
 	
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew doctor
 fi
 
 if ! [ -f "/usr/local/bin/cider" ]; then
