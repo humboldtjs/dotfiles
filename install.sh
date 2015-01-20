8699d48534fab3a726513f0c2dae46fd76dd5d60
Daniël Haveman <daniel@utalk.nl>
2015-01-20 13:25:31 +0100

    Install TextMate

--- install.sh
+++ install.sh
@@ -36,10 +36,43 @@ if ! [ -d ~/.config ]; then
 	ln -s ~/dotfiles/.config ~/.config
 fi
 
+if ! [ -f ~/.gitconfig ]; then
+	ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+fi
+
+if ! [ -f ~/.gitignore ]; then
+	ln -s ~/dotfiles/.gitignore ~/.gitignore
+fi
+
 echo "Cider restore..."
 cider restore
 
 if ! [ "$SHELL" = "/usr/local/bin/fish" ]; then
 	echo "Setting shell to fish..."
+
+	if [ "`grep fish /etc/shells`" = "" ]; then
+		echo /usr/local/bin/fish | sudo tee -a /etc/shells
+	fi
+
 	chsh -s /usr/local/bin/fish
+fi
+
+osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"Pro\""
+osascript -e "tell application \"Terminal\" to set default settings to settings set \"Pro\""
+osascript -e "tell application \"Terminal\" to set startup settings to settings set \"Pro\""
+
+echo "Installing apps..."
+if ! [ -d "/Applications/TextMate.app" ]; then
+	cd ~/dotfiles
+	wget "https://api.textmate.org/downloads/release" -O TextMate.tbz
+
+	mkdir tmp
+	tar -xjf TextMate.tbz -C tmp
+	rm TextMate.tbz
+	
+	sudo mv tmp/TextMate.app /Applications/TextMate.app
+
+	rm -Rf tmp;
+
+	sudo cp /Applications/TextMate.app/Contents/Resources/mate /usr/local/bin/mate
 fi
