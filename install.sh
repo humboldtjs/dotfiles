0e353dedcc6b069ebb9236fa26b495b932aceb70
Daniël Haveman <solitude@Daniels-iMac.local>
2017-10-11 01:45:59 +0200

    Updated to work with new version of homebrew cask, which is now integrated in standard homebrew; and updated some packages to satisfy dependencies, and new locations (android-platform-tools, java, ant). This also meant removing a lot of the testInstall scripts, which are no longer needed.

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+cleanupInstall()
+{
+	if [ -d /opt/homebrew-cask/Caskroom/$1/latest ]; then
+		echo " $1... CLEANUP"
+		brew cask remove $1
+	fi
+}
+
+testInstall()
+{
+	if ! [ -d "/Applications/${1}" ]; then
+		echo " $2... INSTALL"
+		brew cask install $2
+		if ! [ "$3" == "noremove" ]; then
+			LATEST="$3"
+			if [ -z "$3" ]; then
+				LATEST="latest"
+			fi
+			sudo cp -R /opt/homebrew-cask/Caskroom/$2/${LATEST}/*.app /Applications/
+			brew cask remove $2
+		fi
+	else
+		echo " $1... OK"
+	fi
+}
+
+
 ### MAKE SURE OUR PRE-REQUISITES ARE INSTALLED
 echo "PREREQUISITES"
 if ! [ -f "/usr/local/bin/brew" ]; then
@@ -10,12 +37,12 @@ else
 	echo " homebrew... OK"
 fi
 
-if ! [ -f "/usr/local/bin/brew-cask" ]; then
-	echo " brew-cask... INSTALL"
-	brew install caskroom/cask/brew-cask
-else
-	echo " brew-cask... OK"
-fi
+#if ! [ -f "/usr/local/bin/brew-cask" ]; then
+#	echo " brew-cask... INSTALL"
+#	brew install caskroom/cask/brew-cask
+#else
+#	echo " brew-cask... OK"
+#fi
 
 if ! [ -f "/usr/local/bin/pip" ]; then
 	echo " pip... INSTALL"
@@ -62,6 +89,9 @@ linkDotFile "gitconfig"
 linkDotFile "gitignore"
 linkDotFile "nanorc"
 
+echo "UPDATING CASKROOM"
+brew tap caskroom/cask
+
 echo "CIDER RESTORE"
 cider restore
 
@@ -85,48 +115,11 @@ osascript -e "tell application \"Terminal\" to set startup settings to settings 
 
 echo "INSTALL ADDITIONAL APPS"
 
-cleanupInstall()
-{
-	if [ -d /opt/homebrew-cask/Caskroom/$1/latest ]; then
-		echo " $1... CLEANUP"
-		brew cask remove $1
-	fi
-}
-
-testInstall()
-{
-	if ! [ -d "/Applications/${1}" ]; then
-		echo " $2... INSTALL"
-		brew cask install $2
-		if ! [ "$3" == "noremove" ]; then
-			LATEST="$3"
-			if [ -z "$3" ]; then
-				LATEST="latest"
-			fi
-			sudo cp -R /opt/homebrew-cask/Caskroom/$2/${LATEST}/*.app /Applications/
-			brew cask remove $2
-		fi
-	else
-		echo " $1... OK"
-	fi
-}
-
-brew cask install textmate # no longer need to do testInstall because cask installs textmate in the correct place already
-testInstall Dropbox.app dropbox
-testInstall ownCloud.app owncloud noremove
-testInstall Skype.app skype
 cleanupInstall adobe-creative-cloud
 testInstall Adobe\ Creative\ Cloud adobe-creative-cloud noremove
 if ! [ -d "/Applications/Adobe Creative Cloud" ]; then
 	open /opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
 fi
-testInstall Xamarin\ Studio.app xamarin-studio 5.10.0.871-0
-testInstall VLC.app vlc 2.2.1
-testInstall Spotify.app spotify
-testInstall SourceTree.app sourcetree 2.0.5.7
-testInstall Cyberduck.app cyberduck 4.7.3
-testInstall Miro\ Video\ Converter.app miro-video-converter
-testInstall Firefox.app firefox 42.0
 
 FFMPEG=`ffmpeg 2>&1 | grep -i vpx`
 if [ -z "$FFMPEG" ]; then
