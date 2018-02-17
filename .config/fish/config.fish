615f4b034f77da5b5fcc573f048a31c4b276dec2
Daniël Haveman <daniel@utalk.nl>
2018-02-17 13:40:55 +0100

    Added YnabConverter for banking. Added PureRef config. Added ADB to path. Added rust. And a bit of cleanup with the gitignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,1 @@
-set PATH /usr/local/sbin ~/dotfiles/extracommands ~/dotfiles/extracommands/platform-tools $PATH
+set PATH /usr/local/sbin ~/dotfiles/extracommands ~/dotfiles/extracommands/platform-tools ~/.cargo/bin ~/dotfiles/extracommands/google-cloud-sdk/bin $PATH
