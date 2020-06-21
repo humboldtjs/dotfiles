b2fb2a80ad70385cdff2f229c0aad27855b61575
Daniël Haveman <daniel.haveman@indg.com>
2020-06-21 12:27:09 +0200

    bat is not a great default :|

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set PATH ~/dotfiles/extracommands ~/dotfiles/extracommands/platform-tools $PATH
 alias ls='exa --git --header'
-alias cat='bat'
+## alias cat='bat'
 alias top='htop'
+test -s /Users/solitude/.nvm-fish/nvm.fish; and source /Users/solitude/.nvm-fish/nvm.fish
